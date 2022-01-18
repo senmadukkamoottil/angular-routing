7cadf9011cf86296eaf8e5b1fd65dff829577e66
Sen Madukkamoottil <senmadukkamoottil@Sens-MacBook-Air.local>
2022-01-18 17:07:37 -0500

    added child routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
@@ -13,7 +15,14 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {path: 'products/:id', component: ProductDetailComponent},
-      {path: 'products/:id/edit', component: ProductEditComponent}
+      {
+        path: 'products/:id/edit', 
+        component: ProductEditComponent,
+        children: [
+          {path: 'info', component: ProductEditInfoComponent},
+          {path: 'tags', component: ProductEditTagsComponent}
+        ]
+      }
     ])
   ],
   declarations: [
